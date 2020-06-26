68e80d97ce2df202624eecdf7ccb7d471d57e34b
Yash Raj <yashrajsrivastava2312@gmail.com>
2020-06-27 02:14:54 +0530

    initialising build folder

---
+++ build/precache-manifest.80fc24864174430e0832520e9d230a41.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb3187aaa04041eecbcc3c4acef5dcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2728fdd5e9ee245a8f5a",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "90f3fb63f1087356a1b5",
+    "url": "/static/css/main.5fd7b9f2.chunk.css"
+  },
+  {
+    "revision": "2728fdd5e9ee245a8f5a",
+    "url": "/static/js/2.fc170332.chunk.js"
+  },
+  {
+    "revision": "a35346a5ff3029a3521395d4c125dfed",
+    "url": "/static/js/2.fc170332.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90f3fb63f1087356a1b5",
+    "url": "/static/js/main.fef256a2.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "4499ded4ced4b4af9ab33f1ffc542b00",
+    "url": "/static/media/background.4499ded4.jpg"
+  },
+  {
+    "revision": "91a960a04d76d57b14dbe3a6d25176ab",
+    "url": "/static/media/camera.91a960a0.png"
+  }
+]);
